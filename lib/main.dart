03306105dc4733c645eaa4fb826084d7680928aa
yusuffauziyan <yusuffauziyan@gmail.com>
2024-10-04 00:29:22 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Widget'),
-        ),
-        body: const Center(
-          child: Text('Hello, World!'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('My Widget'),
+          ),
+          body: (MediaQuery.of(context).orientation == Orientation.portrait)
+              ? Column(
+                  children: buildContainers(),
+                )
+              : Row(
+                  children: buildContainers(),
+                )),
     );
+  }
+
+  List<Widget> buildContainers() {
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        width: 50,
+        height: 50,
+      ),
+      Container(
+        color: Colors.green,
+        width: 50,
+        height: 50,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 50,
+        height: 50,
+      ),
+      Container(
+        color: Colors.cyan,
+        width: 50,
+        height: 50,
+      )
+    ];
   }
 }
