318b9b70efdc3b8e4e1a36213e001e8a98af4524
yusuffauziyan <yusuffauziyan@gmail.com>
2024-10-03 21:41:13 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,50 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          title: const Text(
-            'App Bar',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          actions: <Widget>[
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                )),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.exit_to_app,
-                  color: Colors.white,
-                )),
-          ],
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: <Color>[
-                  Color(0xff0096ff),
-                  Color(0xff6610f2),
-                ],
-                begin: FractionalOffset.topLeft,
-                end: FractionalOffset.bottomRight,
-              ),
-              image: DecorationImage(
-                image: AssetImage('assets/pattern.png'),
-                fit: BoxFit.cover,
-                repeat: ImageRepeat.repeat,
-              ),
-            ),
-          ),
+          title: const Text('My Widget'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('Hello, World!'),
         ),
       ),
     );
