606daf758af5a024e60fc1c72dacc7f1a482bfba
yusuffauziyan <yusuffauziyan@gmail.com>
2024-10-04 10:36:28 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My Widget'),
+          title: const Text('Button Widget'),
         ),
-        body: const Center(
-          child: Text('Hello, World!'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Elevated Button'),
+              ),
+              Container(
+                  width: 150,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: const LinearGradient(
+                          colors: [Colors.red, Colors.blue],
+                          begin: Alignment.center,
+                          end: Alignment.bottomCenter)),
+                  child: InkWell(
+                    splashColor: Colors.amber,
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(20),
+                    child: const Center(
+                      child: Text('Inkwell Button',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
